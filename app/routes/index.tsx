1ecd49fce9c64bb00d3d78787ff7bff609e27f73
Ari Perkkiö <ari.perkkio@gmail.com>
2022-05-27 18:12:14 +0000

    feat: add cloudflare kv

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,40 @@
+import { redirect } from "@remix-run/cloudflare";
+import type { ActionFunction, LoaderFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { Form, useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async ({ context }) => {
+  return json(await getKv(context).get("count", { type: "text" }));
+};
+
+export const action: ActionFunction = async ({ context }) => {
+  const currentValue = await getKv(context).get("count", { type: "text" });
+  const nextValue = 1 + parseInt(currentValue || "0");
+
+  await getKv(context).put("count", nextValue.toString());
+
+  return redirect("/");
+};
+
 export default function Index() {
+  const count = useLoaderData();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <h1>Remix + Cloudflare Pages + Cloudflare KV</h1>
+      <p>Count is {parseInt(count) || 0}</p>
+
+      <Form method="post">
+        <button type="submit">Increase</button>
+      </Form>
+    </main>
   );
+}
+
+function getKv(context: any) {
+  if (!context || !context.REMIX_CF_PAGES_DEMO_KV) {
+    throw new Error("context.REMIX_CF_PAGES_DEMO_KV not available");
+  }
+
+  return context.REMIX_CF_PAGES_DEMO_KV as KVNamespace;
 }

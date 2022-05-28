5181658a279e2f18f74eaed6e018e278a7ff8510
Ari Perkkiö <ari.perkkio@gmail.com>
2022-05-28 07:50:59 +0000

    feat: style as CRA

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,11 +3,17 @@ import { Form, useLoaderData } from "@remix-run/react";
 import { redirect } from "@remix-run/cloudflare";
 import type { ActionFunction, LoaderFunction } from "@remix-run/cloudflare";
 
+import logo from "../logo.svg";
+import styles from "~/styles/index.css";
 import { getCounter, increaseCounter } from "~/models/counter.server";
 import type { Counter } from "~/models/counter.server";
 
 interface LoaderData {
   counter: Counter;
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
 }
 
 export const loader: LoaderFunction = async ({ context }) => {
@@ -27,12 +33,39 @@ export default function Index() {
 
   return (
     <main>
-      <h1>Remix + Cloudflare Pages + Cloudflare KV</h1>
-      <p>Count is {counter}</p>
+      <img src={logo} alt="logo" />
+
+      <h1>Hello Remix + Cloudflare Pages + Cloudflare KV!</h1>
 
       <Form method="post">
-        <button type="submit">Increase</button>
+        <button type="submit">count is {counter}</button>
       </Form>
+
+      <div className="links">
+        <a
+          href="https://remix.run/docs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Remix Docs
+        </a>
+        {" | "}
+        <a
+          href="https://pages.cloudflare.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cloudflare Pages Docs
+        </a>
+        {" | "}
+        <a
+          href="https://developers.cloudflare.com/workers/runtime-apis/kv/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cloudflare KV Docs
+        </a>
+      </div>
     </main>
   );
 }

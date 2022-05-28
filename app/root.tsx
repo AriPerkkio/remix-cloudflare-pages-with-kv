acda215b5802c71beef54a50e6818d8a7837546f
Ari Perkkiö <ari.perkkio@gmail.com>
2022-05-28 13:55:54 +0000

    feat: replace document title

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Remix deployed on Clouflare Pages utilizing Cloudflare KV",
   viewport: "width=device-width,initial-scale=1",
 });
 
